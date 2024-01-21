6a0a1515004e064ad3785c368a6262569981e609
Lawrence Muchiri <davidmuchiri@Lawrences-MacBook-Pro.local>
2024-01-21 19:54:34 +0300

    Added smooth scroll Anim. using Lenis Animation thx

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ const dateTag = document.getElementById("date")
 const date = new Date()
 
 dateTag.innerText += " " + date.getFullYear()
+
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
